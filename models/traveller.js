3bbf150cd6bd5d13bd9229d6984c59a0546c0098
Steve Gossip <stephengossip@gmail.com>
2019-06-19 16:03:22 +0100

    MVP and totalDistance extension tests passing

---
+++ models/traveller.js
@@ -0,0 +1,42 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const result = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transportToFind) {
+  return this.journeys.filter((journey) => {
+    return (journey.transport === transportToFind);
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return (journey.distance > minDistance);
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
